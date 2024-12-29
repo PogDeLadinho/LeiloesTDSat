433fab593df19ba6a3badd86d70b454c87703660
PogDeLadinho <andreluiz.mattoslopes@hotmail.com>
2024-12-29 10:08:46 -0300

    Configuração da conexão com o banco de dados e criação da main class

---
+++ LeiloesTDSat/LeiloesTDSat/src/MainClass.java
@@ -0,0 +1,14 @@
+
+
+
+public class MainClass {
+    
+    public static void main(String[] args) {
+        
+        cadastroVIEW tela = new cadastroVIEW();
+        tela.setVisible(true);
+        
+        
+    }
+    
+}
